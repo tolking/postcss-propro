0e81c6a458d92b28fd3a044feeb75e99c2e31c41
tolking <qw13131wang@gmail.com>
2018-12-07 18:12:11 +0800

    up data

--- README.md
+++ README.md
@@ -1,29 +1,86 @@
-# PostCSS Tolking [![Build Status][ci-img]][ci]
+# PostCSS Propro [![Build Status][ci-img]][ci]
 
 [PostCSS] plugin test postcss.
 
 [PostCSS]: https://github.com/postcss/postcss
-[ci-img]:  https://travis-ci.org/tolking/postcss-tolking.svg
-[ci]:      https://travis-ci.org/tolking/postcss-tolking
+[ci-img]:  https://travis-ci.org/tolking/postcss-propro.svg
+[ci]:      https://travis-ci.org/tolking/postcss-propro
 
 ## box
-
-```css
+``` css
+// input
 .foo {
   box: 100%;
   min-box: 5rem 2rem;
-  max-box: 10rem;
+  max-box: 10rem 20vh;
 }
 ```
 
-```css
+``` css
+// output
 .foo {
   with: 100%;
-  height: 100%;
+  height: auto;
   min-width: 5rem;
   min-height: 2rem;
   max-width: 10rem;
-  max-height: 10rem;
+  max-height: 20vh;
+}
+```
+
+## arrow
+``` css
+// input
+.foo {
+  arrow: top-left 20px #f0f;
+}
+```
+
+## btn
+``` css
+// input
+.foo {
+  btn: 10rem 4rem 0.5rem #00e;
+}
+```
+
+## font-cc
+``` css
+// input
+.foo {
+  font-cc: 2rem;
+}
+```
+
+## font-hidden
+``` css
+// input
+.foo {
+  font-hidden: 1;
+}
+```
+
+## position
+``` css
+// input
+.foo {
+  position: fixed 0;
+}
+```
+
+## position-cc
+``` css
+// input
+.foo {
+  position-cc: 10rem 4rem;
+}
+```
+
+## spread
+``` css
+// input
+.foo {
+  spread: top-bottom #da3;
 }
 ```
 
@@ -33,7 +90,7 @@
 ## Usage
 
 ```js
-postcss([ require('postcss-tolking') ])
+postcss([ require('postcss-propro') ])
 ```
 
 See [PostCSS] docs for examples for your environment.
--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-var postcss = require('postcss');
+const postcss = require('postcss');
+const arrow = require('./lib/arrow');
+const box = require('./lib/box');
+const btn = require('./lib/btn');
+const fontCC = require('./lib/font-cc');
+const fontHidden = require('./lib/font-hidden');
+const positionCC = require('./lib/position-cc');
+const position = require('./lib/position');
+const spread = require('./lib/spread');
 
-module.exports = postcss.plugin('postcss-tolking',  opts => {
-    opts = opts || {};
-    const boxRegExp = new RegExp('^((?:max|min)-)?box$', 'i');
-  	return root => {
+module.exports = postcss.plugin('postcss-propro',  opts => {
+  	return (root, result) => {
         root.walkRules(rule => {
-            rule.walkDecls(boxRegExp, decl => {
-                const minmax = decl.prop.match(boxRegExp)[1] || '';
-                const values = postcss.list.space(decl.value);
-                const width = values[0];
-                const height = values[1] || values[0];
-
-                decl.cloneBefore({
-                    prop: `${minmax}width`,
-                    value: width
-                });
-                decl.cloneBefore({
-                    prop: `${minmax}height`,
-                    value: height
-                });
-
-                decl.remove();
+            rule.walkDecls(decl => {
+                switch (decl.prop) {
+                case 'arrow':
+                    arrow(decl, opts, result);
+                    break;
+                case 'box':
+                case 'min-box':
+                case 'max-box':
+                    box(decl, opts, result);
+                    break;
+                case 'btn':
+                    btn(decl, opts, result);
+                    break;
+                case 'font-cc':
+                    fontCC(decl, opts, result);
+                    break;
+                case 'font-hidden':
+                    fontHidden(decl, opts, result);
+                    break;
+                case 'position-cc':
+                    positionCC(decl, opts, result);
+                    break;
+                case 'position':
+                    position(decl, opts, result);
+                    break;
+                case 'spread':
+                    spread(decl, opts, result);
+                    break;
+                default:
+                    break;
+                }
             });
         });
   	};
