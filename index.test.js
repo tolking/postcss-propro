085a0304d255fef47df23d3963db720ca14cc165
tolking <qw13131wang@gmail.com>
2019-02-27 20:24:30 +0800

    fix index.test.js

--- index.test.js
+++ index.test.js
@@ -1,5 +1,4 @@
 var postcss = require('postcss');
-
 var plugin = require('./');
 
 function run(input, output, opts) {
@@ -10,10 +9,30 @@ function run(input, output, opts) {
         });
 }
 
-/* Write tests here
-
-it('does something', () => {
-    return run('a{ }', 'a{ }', { });
+it('arrow', () => {
+    return run(
+        'a{ arrow: top 1rem #ccc }',
+        'a{ width: 0; height: 0; border-left: 1rem solid transparent; border-right: 1rem solid transparent; border-bottom: 1rem solid #ccc }'
+    );
 });
 
-*/
+it('box', function () {
+    return run(
+        'a { box: 2rem 1rem }',
+        'a { width: 2rem; height: 1rem }'
+    );
+});
+
+it('font-cc', function () {
+    return run(
+        'a { font-cc: 3rem }',
+        'a { text-align: center; line-height: 3rem }'
+    );
+});
+
+it('position', function () {
+    return run(
+        'a { position: absolute 1rem }',
+        'a { position: absolute; top: 1rem; right: 1rem; bottom: 1rem; left: 1rem }'
+    );
+});
